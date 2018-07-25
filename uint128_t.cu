de6f277ca7a6d09d12f95a2a2ca23d30b3d5e77d
Yorrick <yreinhar@students.uni-mainz.de>
2018-07-25 17:45:03 +0000

    Uploaded uint128_t.cu File

---
+++ uint128_t.cu
@@ -0,0 +1,149 @@
+//
+// Created by Yorrick on 22.05.2018.
+//
+// This is a PSEUDO uint128_t!
+// It won't work as a correct int or decimal representation as I only care about the binary representation in the CARF context.
+#include <string>
+#include <vector>
+#include <sstream> //istringstream
+#include <iostream> // cout
+#include <fstream> // ifstream
+#include <cstring>
+#include <bitset>
+
+using namespace std;
+
+
+struct uint128_t {
+
+    uint64_t LEFT;
+    uint64_t RIGHT;
+
+    __host__ __device__ uint128_t() {
+        LEFT = 0;
+        RIGHT = 0;
+    }
+
+    __host__ __device__ uint128_t(uint64_t right) {
+        LEFT = 0;
+        RIGHT = right;
+    }
+
+    __host__ __device__ uint128_t(uint64_t left, uint64_t right) {
+        LEFT = left;
+        RIGHT = right;
+    }
+
+//    __host__ __device__ uint128_t operator=(const uint128_t &n) const {
+//        return uint128_t(n.LEFT,n.RIGHT);
+//    }
+
+    __host__ __device__ uint128_t operator<<(const size_t &n) const {
+        if(n >= 128){
+            return uint128_t{0, 0};
+        }
+
+        if(n > 64){
+            return uint128_t{(RIGHT<<(n-64)), 0};
+        }
+
+        if(n == 64){
+            return uint128_t{RIGHT, 0};
+        }
+            return uint128_t{(LEFT << n) | (RIGHT >> (64 - n)), RIGHT << n};
+    }
+
+//nochmal pruefen
+    __host__ __device__ uint128_t operator>>(const size_t &n) const {
+	if(n >= 128){
+		return uint128_t{0, 0};
+	}
+	
+	if(n > 64){//TODO nochmal pruefen
+		return uint128_t{0, (LEFT>>(n-64))};
+	}
+	if(n == 64){
+		return uint128_t{0, LEFT};
+	}
+        return uint128_t{(LEFT >> n), (LEFT << (64 - n) | (RIGHT >> n))};
+    }
+
+    __host__ __device__ uint128_t operator|(const uint128_t &n) const {
+        return uint128_t{(n.LEFT | LEFT), (n.RIGHT | RIGHT)};
+    }
+
+    __host__ __device__ uint128_t operator&(const uint128_t &n) const {
+	    return uint128_t{(n.LEFT & LEFT), (n.RIGHT & RIGHT)};
+    }
+
+    __host__ __device__ uint128_t operator^(const uint128_t &n) const {
+        return uint128_t{(n.LEFT^LEFT),(n.RIGHT^RIGHT)};
+    }
+
+    __host__ __device__ bool operator!=(const uint128_t &n) const {
+        if(LEFT==n.LEFT and RIGHT==n.RIGHT){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    __host__ __device__ bool operator==(const uint64_t &n) const {
+        if(LEFT == 0 and RIGHT == n){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    __host__ __device__ bool operator==(const uint128_t &n) const {
+        if(LEFT == n.LEFT and RIGHT == n.RIGHT){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    __host__ __device__ bool operator<(const uint64_t &n) const {
+        if(LEFT > 0){
+            return false;
+        } else if (RIGHT < n){
+            return true;
+        }
+        return false;
+    }
+
+    __host__ __device__ bool operator>(const uint64_t &n) const {
+        if(LEFT > 0){
+            return true;
+        } else if (RIGHT > n){
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+    bitArray getBits(){
+        transform in bitrepresentation
+    */
+
+    __device__ void printC(){
+        printf("%llu , %llu \n", LEFT, RIGHT);
+    }
+
+    __host__ void print() {
+        if(LEFT==0){
+            cout << RIGHT;
+        } else {
+            cout << LEFT << "," << RIGHT;
+        }
+    }
+
+    __host__ void printBits(){
+        std::bitset<64> le = LEFT;
+        std::bitset<64> ri = RIGHT;
+        cout << le <<","<< ri << endl;
+    }
+
+};
